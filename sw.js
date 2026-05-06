85e97db0e11d41c1975303d3accf27109797f7d8
Alessandro Pezzali <pezzalialessandro@gmail.com>
2026-05-06 21:46:29 +0200

    chore(release): bump version per fix font width tabella PDF

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  *   - assets statici           -> stale-while-revalidate
  * CACHE_NAME va bumpato a ogni deploy via ./bump-version.sh
  */
-const CACHE_NAME = 'listoapp-cache-v20260506T193411';
+const CACHE_NAME = 'listoapp-cache-v20260506T194629';
 const SHELL = [
   './',
   './index.html',
