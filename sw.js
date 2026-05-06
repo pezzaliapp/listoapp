6387b15fecc396c5789157f9dcd7d95fedeb6177
Alessandro Pezzali <pezzalialessandro@gmail.com>
2026-05-06 17:51:55 +0200

    chore(release): bump version per attivare intestazione PDF preventivo

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  *   - assets statici           -> stale-while-revalidate
  * CACHE_NAME va bumpato a ogni deploy via ./bump-version.sh
  */
-const CACHE_NAME = 'listoapp-cache-v20260506T141347';
+const CACHE_NAME = 'listoapp-cache-v20260506T155153';
 const SHELL = [
   './',
   './index.html',
