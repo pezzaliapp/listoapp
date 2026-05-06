cb6bda9fe2dfcd7bfa9eebe7d4831396af05d2a6
Alessandro Pezzali <pezzalialessandro@gmail.com>
2026-05-06 18:50:12 +0200

    chore(release): bump version per indent cliente PDF

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  *   - assets statici           -> stale-while-revalidate
  * CACHE_NAME va bumpato a ogni deploy via ./bump-version.sh
  */
-const CACHE_NAME = 'listoapp-cache-v20260506T163125';
+const CACHE_NAME = 'listoapp-cache-v20260506T165012';
 const SHELL = [
   './',
   './index.html',
