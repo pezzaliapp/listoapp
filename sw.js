447f4fbe8e5104fb7787414dccd713878d0d2c8b
Alessandro Pezzali <pezzalialessandro@gmail.com>
2026-05-06 21:27:30 +0200

    chore(release): bump version per Patch B PDF professionale

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  *   - assets statici           -> stale-while-revalidate
  * CACHE_NAME va bumpato a ogni deploy via ./bump-version.sh
  */
-const CACHE_NAME = 'listoapp-cache-v20260506T190436';
+const CACHE_NAME = 'listoapp-cache-v20260506T192730';
 const SHELL = [
   './',
   './index.html',
